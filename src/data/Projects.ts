d425658cf5f73f213f1b54cff9662ab5edf3ba49
Loaf <90423308+ponderingdemocritus@users.noreply.github.com>
2024-12-30 10:38:26 +1100

    Update Projects.ts

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -6,7 +6,7 @@ export const links = [
     twitterRealms: 'https://twitter.com/LootRealms',
     twitterBiblio: 'https://twitter.com/BibliothecaDAO',
     website: 'https://realms.world',
-    snapshot: 'https://snapshot.org/#/council.bibliotheca.eth',
+    snapshot: 'https://snapshot.box/#/sn:0x07bd3419669f9f0cc8f19e9e2457089cdd4804a4c41a5729ee9c7fd02ab8ab62',
     scroll: 'https://scroll.bibliothecadao.xyz/',
     staking: 'https://staking.bibliothecadao.xyz/',
     treasury: '/treasury',
